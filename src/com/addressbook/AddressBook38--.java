fe86828bc735aedd178cb5035e5d1e6fd8a4a2b2
Shilpa-K07 <70196456+Shilpa-K07@users.noreply.github.com>
2023-06-29 17:09:48 +0530

    Update and rename AddressBook38.java to AddressBook38--.java

--- src/com/addressbook/AddressBook38--.java
+++ src/com/addressbook/AddressBook38--.java
@@ -5,12 +5,6 @@ import java.util.Scanner;
 import java.util.Scanner;
 import java.util.Scanner;
 import java.util.Scanner;
-import java.util.Scanner;
-import java.util.Scanner;
-import java.util.Scanner;
-import java.util.Scanner;
-import java.util.Scanner;
-import java.util.Scanner;
 
 public class AddressBook {
 	public static void main(String[] args) {
